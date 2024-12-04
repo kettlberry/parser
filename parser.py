c01eeb5195f50cb4b9074dee24efdd10e2ed4913
Aleksandr Katlobay <71895070+kettlberry@users.noreply.github.com>
2024-12-04 20:44:34 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,32 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
 
-def parse_magput(url):
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+}
+
+def parse_magput(url, headers):
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         response.raise_for_status()
         soup = BeautifulSoup(response.content, "html.parser")
 
-        tour_elements = soup.select(".tcart__item") # Используем CSS селектор
+        tour_elements = soup.select(".t-card")  # Исправленный селектор
+
+        print(f"Найдено туров: {len(tour_elements)}", flush=True) # Проверка количества найденных элементов
 
         for tour_element in tour_elements:
             link_element = tour_element.find("a", target="_blank", href=True)
             direction_element = tour_element.find("div", class_="tcart__direction")
 
+
             if link_element and direction_element:
-                link = "https://magput.ru" + link_element["href"] # Добавляем базовый URL
+                link = "https://magput.ru" + link_element["href"]
                 direction = direction_element.text.strip()
-                print(f"Ссылка: {link}, Направление: {direction}")
+                print(f"Ссылка: {link}, Направление: {direction}", flush=True)
             else:
-                print("Ошибка: не удалось найти ссылку или направление для одного из туров.")
+                print("Ошибка: не удалось найти ссылку или направление для одного из туров.", flush=True)
 
     except requests.exceptions.RequestException as e:
-        print(f"Ошибка при запросе к {url}: {e}")
+        print(f"Ошибка при запросе к {url}: {e}", flush=True)
     except Exception as e:
-        print(f"Неизвестная ошибка: {e}")
-
+        print(f"Неизвестная ошибка: {e}", flush=True)
 
 
 if __name__ == "__main__":
     url = "https://magput.ru/tours/india"
-    parse_magput(url)
+    parse_magput(url, headers)
