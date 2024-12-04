35eda5f250bff06dcc924a4943a00ec45b2e971b
Aleksandr Katlobay <71895070+kettlberry@users.noreply.github.com>
2024-12-04 18:48:24 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -5,56 +5,35 @@ import re
 base_url = "https://vand.ru/country/india/"
 tour_base_url = "https://vand.ru"
 
-
 def parse_tour_page(tour_url):
-    try:
-        response = requests.get(tour_url)
-        response.raise_for_status()  # Проверка на ошибки HTTP
-        soup = BeautifulSoup(response.content, "html.parser")
-
-        title_element = soup.find("h1", class_="page-title h1")
-        description_element = soup.find("div", class_="my-4 text-center")
-
-        title = title_element.text.strip() if title_element else ""
-        description = description_element.text.strip() if description_element else ""
-
-        return title, description
-    except requests.exceptions.RequestException as e:
-        print(f"Ошибка при запросе к {tour_url}: {e}")
-        return "", ""
-
-
+    # ... (Этот код остается без изменений)
 
 def parse_page(page_url):
-    try:
-        response = requests.get(page_url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.content, "html.parser")
-
-        tour_links = soup.find_all("a", href=re.compile(r"/country/india/tour/"))
-
-        for tour_link in tour_links:
-            href = tour_link["href"]
-            tour_url = tour_base_url + href
-            title, description = parse_tour_page(tour_url)
-            print(f"{tour_url},{title},{description}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Ошибка при запросе к {page_url}: {e}")
+    # ... (Этот код остается без изменений)
 
 
 def main():
     page_num = 1
     while True:
-      page_url = f"{base_url}?PAGEN_1={page_num}"
-      response = requests.get(page_url)
+        page_url = f"{base_url}?PAGEN_1={page_num}"
+        response = requests.get(page_url)
 
-      if response.status_code == 404: # Проверка на существование страницы
-          break
+        if response.status_code == 404:
+            break  # Выход из цикла, если страницы больше нет
 
-      parse_page(page_url)
-      page_num += 1
+        soup = BeautifulSoup(response.content, 'html.parser')
+        # Проверяем наличие блока пагинации
+        pagination_block = soup.find('div', class_='bx-pagination-container')
+        if pagination_block:
+          # Ищем ссылку на следующую страницу
+          next_link = pagination_block.find('li', class_='bx-pag-next')
+          # Если ссылки на следующую страницу нет, значит, это последняя страница
+          if not next_link or not next_link.find('a'):
+              parse_page(page_url) # Парсим последнюю страницу
+              break
 
+        parse_page(page_url)
+        page_num += 1
 
 
 if __name__ == "__main__":
